5d54ab6a674530cfec49c540e5a59da9e12444c8
devsaurabhs <devs.saurabhs@gmail.com>
2018-08-16 12:58:46 +0530

    redux code added

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+ return[
+     {'title':'React JS','auther':'Saurabh Sharma'},
+     {'title':'Angular JS','auther':'Saurabh Sharma1'},
+     {'title':'Node JS','auther':'Saurabh Sharma2'},
+     {'title':'Python JS','auther':'Saurabh Sharma3'},
+     {'title':'PHP','auther':'Saurabh Sharma4'},
+ ]
+
+}
